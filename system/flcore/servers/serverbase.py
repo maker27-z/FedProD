1c2ae5f3ed22a3264e1ad130c4072a5941f44999
maker27-z <zshaifeng1@gmail.com>
2026-07-14 19:52:24 +0800

    Add files via upload

---
+++ system/flcore/servers/serverbase.py
@@ -0,0 +1,418 @@
+import torch
+import os
+import numpy as np
+import h5py
+import copy
+import time
+import random
+from utils.data_utils import load_and_preprocess_data
+from utils.dlg import DLG
+
+
+class Server(object):
+    def __init__(self, args, times):
+        # Set up the main attributes
+        self.args = args
+        self.device = args.device
+        self.dataset = args.dataset
+        self.num_classes = args.num_classes
+        self.global_rounds = args.global_rounds
+        self.local_epochs = args.local_epochs
+        self.batch_size = args.batch_size
+        self.learning_rate = args.local_learning_rate
+        self.global_model = copy.deepcopy(args.model)
+        self.num_clients = args.num_clients
+        self.join_ratio = args.join_ratio
+        self.random_join_ratio = args.random_join_ratio
+        self.num_join_clients = int(self.num_clients * self.join_ratio)
+        self.current_num_join_clients = self.num_join_clients
+        self.few_shot = args.few_shot
+        self.algorithm = args.algorithm
+        self.time_select = args.time_select
+        self.goal = args.goal
+        self.time_threthold = args.time_threthold
+        self.save_folder_name = args.save_folder_name
+        self.top_cnt = args.top_cnt
+        self.auto_break = args.auto_break
+        self.d_alpha = args.d_alpha
+        self.alpha_param = args.alpha_param
+        self.beta_param = args.beta_param
+
+
+        self.clients = []
+        self.selected_clients = []
+        self.train_slow_clients = []
+        self.send_slow_clients = []
+
+        self.uploaded_weights = []
+        self.uploaded_ids = []
+        self.uploaded_models = []
+
+        self.rs_test_acc = []
+        self.rs_test_auc = []
+        self.rs_train_loss = []
+
+        self.times = times
+        self.eval_gap = args.eval_gap
+        self.client_drop_rate = args.client_drop_rate
+        self.train_slow_rate = args.train_slow_rate
+        self.send_slow_rate = args.send_slow_rate
+
+        self.dlg_eval = args.dlg_eval
+        self.dlg_gap = args.dlg_gap
+        self.batch_num_per_client = args.batch_num_per_client
+
+        self.num_new_clients = args.num_new_clients
+        self.new_clients = []
+        self.eval_new_clients = False
+        self.fine_tuning_epoch_new = args.fine_tuning_epoch_new
+        self.load_dataset()
+    def set_clients(self, clientObj):
+        for i, train_slow, send_slow in zip(range(self.num_clients), self.train_slow_clients, self.send_slow_clients):
+            client = clientObj(self.args, 
+                            id=i, 
+                            train_samples=len(self.client_train_dataloaders[i]), 
+                            test_samples=len(self.test_dataloader), 
+                            train_slow=train_slow, 
+                            send_slow=send_slow)
+            self.clients.append(client)
+
+    # random select slow clients
+    def select_slow_clients(self, slow_rate):
+        slow_clients = [False for i in range(self.num_clients)]
+        idx = [i for i in range(self.num_clients)]
+        idx_ = np.random.choice(idx, int(slow_rate * self.num_clients))
+        for i in idx_:
+            slow_clients[i] = True
+
+        return slow_clients
+
+    def set_slow_clients(self):
+        self.train_slow_clients = self.select_slow_clients(
+            self.train_slow_rate)
+        self.send_slow_clients = self.select_slow_clients(
+            self.send_slow_rate)
+
+    def select_clients(self):
+        if self.random_join_ratio:
+            self.current_num_join_clients = np.random.choice(range(self.num_join_clients, self.num_clients+1), 1, replace=False)[0]
+        else:
+            self.current_num_join_clients = self.num_join_clients
+        selected_clients = list(np.random.choice(self.clients, self.current_num_join_clients, replace=False))
+
+        return selected_clients
+
+    def send_models(self):
+        assert (len(self.clients) > 0)
+
+        for client in self.clients:
+            start_time = time.time()
+            
+            client.set_parameters(self.global_model)
+
+            client.send_time_cost['num_rounds'] += 1
+            client.send_time_cost['total_cost'] += 2 * (time.time() - start_time)
+
+    def receive_models(self):
+        assert (len(self.selected_clients) > 0)
+
+        active_clients = random.sample(
+            self.selected_clients, int((1-self.client_drop_rate) * self.current_num_join_clients))
+
+        self.uploaded_ids = []
+        self.uploaded_weights = []
+        self.uploaded_models = []
+        tot_samples = 0
+        for client in active_clients:
+            try:
+                client_time_cost = client.train_time_cost['total_cost'] / client.train_time_cost['num_rounds'] + \
+                        client.send_time_cost['total_cost'] / client.send_time_cost['num_rounds']
+            except ZeroDivisionError:
+                client_time_cost = 0
+            if client_time_cost <= self.time_threthold:
+                tot_samples += client.train_samples
+                self.uploaded_ids.append(client.id)
+                self.uploaded_weights.append(client.train_samples)
+                self.uploaded_models.append(client.model)
+        for i, w in enumerate(self.uploaded_weights):
+            self.uploaded_weights[i] = w / tot_samples
+
+    def aggregate_parameters(self):
+        assert (len(self.uploaded_models) > 0)
+
+        self.global_model = copy.deepcopy(self.uploaded_models[0])
+        for param in self.global_model.parameters():
+            param.data.zero_()
+            
+        for w, client_model in zip(self.uploaded_weights, self.uploaded_models):
+            self.add_parameters(w, client_model)
+
+    def add_parameters(self, w, client_model):
+        for server_param, client_param in zip(self.global_model.parameters(), client_model.parameters()):
+            server_param.data += client_param.data.clone() * w
+
+    def save_global_model(self):
+        model_path = os.path.join("models", self.dataset)
+        if not os.path.exists(model_path):
+            os.makedirs(model_path)
+        model_path = os.path.join(model_path, self.algorithm + "_server" + ".pt")
+        torch.save(self.global_model, model_path)
+
+    def load_model(self):
+        model_path = os.path.join("models", self.dataset)
+        model_path = os.path.join(model_path, self.algorithm + "_server" + ".pt")
+        assert (os.path.exists(model_path))
+        self.global_model = torch.load(model_path)
+
+    def model_exists(self):
+        model_path = os.path.join("models", self.dataset)
+        model_path = os.path.join(model_path, self.algorithm + "_server" + ".pt")
+        return os.path.exists(model_path)
+        
+    def save_results(self):
+        algo = self.dataset + "_" + self.algorithm
+        result_path = "../results/"
+        if not os.path.exists(result_path):
+            os.makedirs(result_path)
+
+        if (len(self.rs_test_acc)):
+            algo = algo + "_" + self.goal + "_" + str(self.times)
+            file_path = result_path + "{}.h5".format(algo)
+            print("File path: " + file_path)
+
+            with h5py.File(file_path, 'w') as hf:
+                hf.create_dataset('rs_test_acc', data=self.rs_test_acc)
+                hf.create_dataset('rs_test_auc', data=self.rs_test_auc)
+                hf.create_dataset('rs_train_loss', data=self.rs_train_loss)
+
+    def save_item(self, item, item_name):
+        if not os.path.exists(self.save_folder_name):
+            os.makedirs(self.save_folder_name)
+        torch.save(item, os.path.join(self.save_folder_name, "server_" + item_name + ".pt"))
+
+    def load_item(self, item_name):
+        return torch.load(os.path.join(self.save_folder_name, "server_" + item_name + ".pt"))
+
+    def test_metrics(self):
+        if self.eval_new_clients and self.num_new_clients > 0:
+            self.fine_tuning_new_clients()
+            return self.test_metrics_new_clients()
+        
+        acc_list = []
+        pre_list = []
+        recall_list = []
+        f1_score_list = []
+        test_loss_list = []
+
+        for c in self.clients:
+            accuracy, precision, recall, f1_score, test_loss  = c.test_metrics(self.test_dataloader)
+            acc_list.append(accuracy)
+            pre_list.append(precision)
+            recall_list.append(recall)
+            f1_score_list.append(f1_score)
+            test_loss_list.append(test_loss)
+
+        return acc_list, pre_list, recall_list, f1_score_list, test_loss_list
+
+    def train_metrics(self):
+        if self.eval_new_clients and self.num_new_clients > 0:
+            return [0], [1], [0]
+        
+        num_samples = []
+        losses = []
+        for c in self.clients:
+            cl, ns = c.train_metrics(self.client_train_dataloaders[c.id])
+            num_samples.append(ns)
+            losses.append(cl*1.0)
+
+        ids = [c.id for c in self.clients]
+
+        return ids, num_samples, losses
+
+    # evaluate selected clients
+    def evaluate(self, acc=None, loss=None):
+        acc_list, pre_list, recall_list, f1_score_list, test_loss_list = self.test_metrics()
+        test_acc = sum(acc_list) /len(acc_list)
+        test_pre = sum(pre_list) /len(pre_list)
+        test_recall = sum(recall_list) /len(recall_list)
+        test_f1_score = sum(f1_score_list) /len(f1_score_list)
+        test_loss = sum(test_loss_list) /len(test_loss_list)
+
+        # Compute global confusion matrix and per-class metrics
+        global_cm = np.zeros((self.num_classes, self.num_classes), dtype=np.int64)
+        for c in self.clients:
+            if hasattr(c, 'test_conf_matrix'):
+                global_cm += c.test_conf_matrix
+        self._last_global_cm = global_cm
+
+        print("Averaged Test Accuracy: {:.4f}".format(test_acc))
+        print("Averaged Test Precision: {:.4f}".format(test_pre))
+        print("Averaged Test Recall: {:.4f}".format(test_recall))
+        print("Averaged Test F1 Score: {:.4f}".format(test_f1_score))
+        print("Averaged Test Loss: {:.6f}".format(test_loss))
+
+        # self.print_(test_acc, train_acc, train_loss)
+        print("Std Test Accuracy: {:.4f}".format(np.std(acc_list)))
+        print("Std Test Precision: {:.4f}".format(np.std(pre_list)))
+        print("Std Test Recall: {:.4f}".format(np.std(recall_list)))
+        print("Std Test F1 Score: {:.4f}".format(np.std(f1_score_list)))
+        return test_acc, test_pre, test_recall, test_f1_score, test_loss
+
+    def save_detailed_metrics(self, round_num):
+        import json
+        if not hasattr(self, '_last_global_cm'):
+            print("No confusion matrix to save.")
+            return
+        save_dir = f"./detailed_metrics/{self.dataset}/{self.algorithm}"
+        os.makedirs(save_dir, exist_ok=True)
+
+        cm = self._last_global_cm
+        np.save(os.path.join(save_dir, f"confusion_matrix_round_{round_num}.npy"), cm)
+
+        per_class_p = np.array([
+            cm[i, i] / cm[:, i].sum() if cm[:, i].sum() > 0 else 0.0
+            for i in range(self.num_classes)
+        ])
+        per_class_r = np.array([
+            cm[i, i] / cm[i, :].sum() if cm[i, :].sum() > 0 else 0.0
+            for i in range(self.num_classes)
+        ])
+        per_class_f1 = np.array([
+            2 * p * r / (p + r) if (p + r) > 0 else 0.0
+            for p, r in zip(per_class_p, per_class_r)
+        ])
+
+        metrics_dict = {
+            "round": round_num,
+            "num_classes": self.num_classes,
+            "confusion_matrix": cm.tolist(),
+            "per_class_precision": per_class_p.tolist(),
+            "per_class_recall": per_class_r.tolist(),
+            "per_class_f1": per_class_f1.tolist(),
+        }
+
+        with open(os.path.join(save_dir, f"per_class_metrics_round_{round_num}.json"), 'w') as f:
+            json.dump(metrics_dict, f, indent=2)
+
+        print(f"Detailed metrics saved to {save_dir}")
+
+
+    def print_(self, test_acc, test_auc, train_loss):
+        print("Average Test Accuracy: {:.4f}".format(test_acc))
+        print("Average Test AUC: {:.4f}".format(test_auc))
+        print("Average Train Loss: {:.4f}".format(train_loss))
+
+    def check_done(self, acc_lss, top_cnt=None, div_value=None):
+        for acc_ls in acc_lss:
+            if top_cnt is not None and div_value is not None:
+                find_top = len(acc_ls) - torch.topk(torch.tensor(acc_ls), 1).indices[0] > top_cnt
+                find_div = len(acc_ls) > 1 and np.std(acc_ls[-top_cnt:]) < div_value
+                if find_top and find_div:
+                    pass
+                else:
+                    return False
+            elif top_cnt is not None:
+                find_top = len(acc_ls) - torch.topk(torch.tensor(acc_ls), 1).indices[0] > top_cnt
+                if find_top:
+                    pass
+                else:
+                    return False
+            elif div_value is not None:
+                find_div = len(acc_ls) > 1 and np.std(acc_ls[-top_cnt:]) < div_value
+                if find_div:
+                    pass
+                else:
+                    return False
+            else:
+                raise NotImplementedError
+        return True
+
+    def call_dlg(self, R):
+        # items = []
+        cnt = 0
+        psnr_val = 0
+        for cid, client_model in zip(self.uploaded_ids, self.uploaded_models):
+            client_model.eval()
+            origin_grad = []
+            for gp, pp in zip(self.global_model.parameters(), client_model.parameters()):
+                origin_grad.append(gp.data - pp.data)
+
+            target_inputs = []
+            trainloader = self.clients[cid].load_train_data()
+            with torch.no_grad():
+                for i, (x, y) in enumerate(trainloader):
+                    if i >= self.batch_num_per_client:
+                        break
+
+                    if type(x) == type([]):
+                        x[0] = x[0].to(self.device)
+                    else:
+                        x = x.to(self.device)
+                    y = y.to(self.device)
+                    output = client_model(x)
+                    target_inputs.append((x, output))
+
+            d = DLG(client_model, origin_grad, target_inputs)
+            if d is not None:
+                psnr_val += d
+                cnt += 1
+            
+            # items.append((client_model, origin_grad, target_inputs))
+                
+        if cnt > 0:
+            print('PSNR value is {:.2f} dB'.format(psnr_val / cnt))
+        else:
+            print('PSNR error')
+
+        # self.save_item(items, f'DLG_{R}')
+
+    def set_new_clients(self, clientObj):
+        for i in range(self.num_clients, self.num_clients + self.num_new_clients):
+            
+            client = clientObj(self.args, 
+                            id=i, 
+                            train_samples=len(self.client_train_dataloaders[i]), 
+                            test_samples=len(self.test_dataloader), 
+                            train_slow=False, 
+                            send_slow=False)
+            self.new_clients.append(client)
+
+    # fine-tuning on new clients
+    def fine_tuning_new_clients(self):
+        for client in self.new_clients:
+            client.set_parameters(self.global_model)
+            opt = torch.optim.SGD(client.model.parameters(), lr=self.learning_rate)
+            CEloss = torch.nn.CrossEntropyLoss()
+            trainloader = client.load_train_data()
+            client.model.train()
+            for e in range(self.fine_tuning_epoch_new):
+                for i, (x, y) in enumerate(trainloader):
+                    if type(x) == type([]):
+                        x[0] = x[0].to(client.device)
+                    else:
+                        x = x.to(client.device)
+                    y = y.to(client.device)
+                    output = client.model(x)
+                    loss = CEloss(output, y)
+                    opt.zero_grad()
+                    loss.backward()
+                    opt.step()
+
+    # evaluating on new clients
+    def test_metrics_new_clients(self):
+        num_samples = []
+        tot_correct = []
+        tot_auc = []
+        for c in self.new_clients:
+            ct, ns, auc = c.test_metrics()
+            tot_correct.append(ct*1.0)
+            tot_auc.append(auc*ns)
+            num_samples.append(ns)
+
+        ids = [c.id for c in self.new_clients]
+
+        return ids, num_samples, tot_correct, tot_auc
+    
+    def load_dataset(self):
+        self.client_train_dataloaders, self.val_dataloader, self.test_dataloader = load_and_preprocess_data(
+            dataset_name=self.dataset,num_clients=self.num_clients,d_alpha=self.d_alpha,alpha=self.alpha_param,beta_param=self.beta_param,batch_size=self.batch_size)
